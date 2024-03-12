7548e14b4735138e3d8f71701a7e6b780dad42bc
javier.torreantunez@jgvaldemora.org <javier.torreantunez@jgvaldemora.org>
2024-03-12 21:18:36 +0100

    Commit remoto

--- Ejercicios/src/paquete1/Principal.java
+++ Ejercicios/src/paquete1/Principal.java
@@ -5,6 +5,6 @@ public class Principal {
     public static void main (String[] args) {
         System.out.println ("Estamos haciendo control de versiones con Git");
         System.out.println ("Escribo otra línea para hacer un 2º commit");
-        System.out.println ("Escribo otra línea para hacer commit remoto");
+        System.out.println ("Escribo una línea para hacer commit remoto");
     }
 }
